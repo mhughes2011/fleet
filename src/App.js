f1872ad29e76019db60726b817e7c85ddeaf5f97
Max Hughes <mhughes2011@gmail.com>
2022-04-03 10:27:16 -0700

    implemented base react styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react'
+import './App.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 import Home from './pages/Home';
 
 function App() {
   return (
-    <>
+    <div className='App'>
       <Router>
         <Routes>
           <Route path='/' element={<Home />} />
         </Routes>
       </Router>
-    </>
+    </div>
   );
 }
 
