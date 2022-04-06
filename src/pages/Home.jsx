873207a4e7492171d64159e4ab450095feda3cef
Max Hughes <mhughes2011@gmail.com>
2022-04-05 18:10:40 -0700

    brought in and set up tailwindcss

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>
-      So you want to reduce your Fleet Emissions huh?
-    </div>
+    <>
+      <div>
+        <h1>So you want to reduce your Fleet Emissions...</h1>
+      </div>
+    
+
+      <div className=''>
+        <div className="col1">
+          <h2>Every gallon of gasoline burned produces 19 lbs of CO<sub>2</sub></h2>
+        </div>
+        <div className="col2">
+          <h2>Every gallon of diesel burned produces  22.38 lbs of CO<sub>2</sub></h2>
+        </div>
+      </div>
+
+      <div>
+        <h1>One therm of natural gas burned produces 11.7 lbs of CO<sub>2</sub></h1>
+      </div>
+    </>
   )
 }
 
